4b9a6c43efb6d412bbd4183754ec0cd66bc1c3b6
SGPublic <3300633844@qq.com>
2020-11-19 17:51:51 +0800

    first commit

--- js/data/FailedMarkData.ts
+++
@@ -1,9 +0,0 @@
-export class FailedMarkData {
-    public readonly name;
-    public readonly mark;
-
-    constructor(name: string, mark: string) {
-        this.name = name;
-        this.mark = mark;
-    }
-}
--- js/index.ts
+++
@@ -1,9 +0,0 @@
-export function onDrawerListItemClick(index: number){
-    const active_list: HTMLCollectionOf<Element> = document.getElementsByClassName("mdui-list-item-active");
-    for (let active_index = 0; active_index < active_list.length; active_index++){
-        const active_item: Element = active_list.item(active_index);
-        active_item.classList.remove("mdui-list-item-active");
-    }
-    const drawer_list: HTMLCollectionOf<Element> = document.getElementsByClassName("drawer-list");
-    drawer_list.item(index).classList.add("mdui-list-item-active");
-}
