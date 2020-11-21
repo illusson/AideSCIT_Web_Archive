f336d884178d408b18fb79328f30bcf9bba2547f
SGPublic <3300633844@qq.com>
2020-11-21 18:30:10 +0800

    first commit.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.onDrawerListItemClick = void 0;
-function onDrawerListItemClick(index) {
-    var active_list = document.getElementsByClassName("mdui-list-item-active");
-    for (var active_index = 0; active_index < active_list.length; active_index++) {
-        var active_item = active_list.item(active_index);
-        active_item.classList.remove("mdui-list-item-active");
+var DrawerController = /** @class */ (function () {
+    function DrawerController() {
     }
-    var drawer_list = document.getElementsByClassName("drawer-list");
-    drawer_list.item(index).classList.add("mdui-list-item-active");
+    DrawerController.index_now = -1;
+    return DrawerController;
+}());
+function onDrawerListItemClick(index) {
+    if (index == 4) {
+        window.open("http://218.6.163.95:8081/");
+    }
+    else {
+        var active_list = document.getElementsByClassName("mdui-list-item-active");
+        for (var active_index = 0; active_index < active_list.length; active_index++) {
+            var active_item = active_list.item(active_index);
+            active_item.classList.remove("mdui-list-item-active");
+        }
+        var drawer_list = document.getElementsByClassName("drawer-list");
+        drawer_list.item(index).classList.add("mdui-list-item-active");
+    }
+    if (index != DrawerController.index_now) {
+        var page_list = document.getElementsByClassName("index-page");
+        for (var active_index = 0; active_index < page_list.length; active_index++) {
+            var active_item_1 = page_list.item(active_index);
+            active_item_1.setAttribute("style", "display: none;");
+        }
+        var active_item = page_list.item(index);
+        if (active_item.hasAttribute("style")) {
+            active_item.removeAttribute("style");
+        }
+    }
+    DrawerController.index_now = index;
 }
 exports.onDrawerListItemClick = onDrawerListItemClick;
+window.onload = function () {
+    onDrawerListItemClick(0);
+};
 //# sourceMappingURL=index.js.map
--- js/index.ts
+++ js/index.ts
@@ -1,9 +1,33 @@
+class DrawerController {
+    public static index_now = -1;
+}
+
 export function onDrawerListItemClick(index: number){
-    const active_list: HTMLCollectionOf<Element> = document.getElementsByClassName("mdui-list-item-active");
-    for (let active_index = 0; active_index < active_list.length; active_index++){
-        const active_item: Element = active_list.item(active_index);
-        active_item.classList.remove("mdui-list-item-active");
+    if (index == 4){
+        window.open("http://218.6.163.95:8081/");
+    } else {
+        const active_list: HTMLCollectionOf<Element> = document.getElementsByClassName("mdui-list-item-active");
+        for (let active_index = 0; active_index < active_list.length; active_index++){
+            const active_item: Element = active_list.item(active_index);
+            active_item.classList.remove("mdui-list-item-active");
+        }
+        const drawer_list: HTMLCollectionOf<Element> = document.getElementsByClassName("drawer-list");
+        drawer_list.item(index).classList.add("mdui-list-item-active");
     }
-    const drawer_list: HTMLCollectionOf<Element> = document.getElementsByClassName("drawer-list");
-    drawer_list.item(index).classList.add("mdui-list-item-active");
+    if (index != DrawerController.index_now) {
+        const page_list: HTMLCollectionOf<Element> = document.getElementsByClassName("index-page");
+        for (let active_index = 0; active_index < page_list.length; active_index++){
+            const active_item: Element = page_list.item(active_index);
+            active_item.setAttribute("style", "display: none;")
+        }
+        const active_item = page_list.item(index);
+        if (active_item.hasAttribute("style")) {
+            active_item.removeAttribute("style");
+        }
+    }
+    DrawerController.index_now = index;
+}
+
+window.onload = function (){
+    onDrawerListItemClick(0);
 }
